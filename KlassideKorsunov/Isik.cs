c676f116ad13df3fe78cc7872e007217c91e939c
Andreikorsunov <andrei.korshunov2004@gmail.com>
2022-09-07 00:46:52 +0300

    Isik

--- KlassideKorsunov/Isik.cs
+++ KlassideKorsunov/Isik.cs
@@ -12,9 +12,45 @@ namespace KlassideKorsunov
         protected int synniAasta;
         public enum sugu { mees, naine };
         protected sugu Sugu;
-        public Isik(string nimi, int synniAasta, sugu Sugu) 
-        { 
+        public string koolinimi;
+        public string klass;
+        public string specialsus;
+        
+        public string asutus;
+        public string amet;
+        public double tootasu;
+
+        public string oppeasutus;
+        public string eriala;
+        public int kursus;
+        public Isik(string nimi, int synniAasta, sugu Sugu, string kooliNimi, string klass, string specialsus)
+        {
+            this.nimi = nimi;
+            this.synniAasta = synniAasta;
             this.Sugu = Sugu;
+            this.koolinimi = kooliNimi;
+            this.klass = klass;
+            this.specialsus = specialsus;
+        }
+
+        public Isik(string nimi, int synniAasta, sugu Sugu, string asutus, string amet, double tootasu)
+        {
+            this.nimi = nimi;
+            this.synniAasta = synniAasta;
+            this.Sugu = Sugu;
+            this.asutus = asutus;
+            this.amet = amet;
+            this.tootasu = tootasu;
+        }
+
+        public Isik(string nimi, int synniAasta, sugu Sugu, string oppeasutus, string eriala, int kursus)
+        {
+            this.nimi = nimi;
+            this.synniAasta = synniAasta;
+            this.Sugu = Sugu;
+            this.oppeasutus = oppeasutus;
+            this.eriala = eriala;
+            this.kursus = kursus;
         }
         public Isik(Isik isik)
         {
@@ -24,7 +60,7 @@ namespace KlassideKorsunov
         }
         public virtual void print_Info()
         {
-            Console.Write("{nimi} sündinud {synniAasta} aastal, ja tema sugu on {Sugu}");
+            Console.Write("{nimi} sündinud {synniAasta} aastal, ja ta on {Sugu}");
         }
         public void muudaNimi(string uusNimi) { nimi = uusNimi; }
         public void muudaSynniAasta(int uusSynniAasta) { synniAasta = uusSynniAasta; }
